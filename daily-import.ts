7360d912c60cd93f48b801b059327dbd6d563e8b
Derek Wene <derek@lambdacurry.dev>
2025-04-25 07:25:48 -0500

    chore: cleanup linting

--- daily-import.ts
+++ daily-import.ts
@@ -1,7 +1,7 @@
 import { importData } from './import-data';
 import { Resource } from 'sst';
 export default {
-	async scheduled(event: unknown, env: unknown, ctx: unknown) {
-		await importData(Resource.AddressValidationDB);
-	},
+  async scheduled(event: unknown, env: unknown, ctx: unknown) {
+    await importData(Resource.AddressValidationDB);
+  },
 };
